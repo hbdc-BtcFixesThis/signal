657c01ff6a8b42b8da6c7eb85cedc8866687c98f
hbdc <hbdc.bitcoinFixesThis@proton.me>
2023-05-25 15:46:02 -0400

    remove navbar; replace nav with key to unlock settings; make pct  slider reuseable; small clenup

--- static/js/percent_slider.js
+++ static/js/percent_slider.js
@@ -1,24 +1,42 @@
-var percentInput = document.querySelector("#percent-slider-input");
+var percentNewRecordInput = document.querySelector("#percent-slider-input");
+var newRecordSliderBar1 = document.getElementById("percent-slider-new-rec-1");
+var newRecordSliderBar2 = document.getElementById("percent-slider-new-rec-2");
 
-function initPercentInput() {
-	var sliderBars = document.querySelector(".percent-slider-bars");
-	percentInput.style.height = getComputedStyle(sliderBars).height;
-	percentInput.style.bottom = getComputedStyle(sliderBars).height
+var percentSliderStorage = document.querySelector("#percent-slider-storage");
+var serverStorageSliderBar1 = document.getElementById("percent-slider-server-storage-1");
+var serverStorageSliderBar2 = document.getElementById("percent-slider-server-storage-2");
+
+
+function initSliderPercentInput(sliderBar, elem) {
+	elem.style.height = getComputedStyle(sliderBar).height;
+	elem.style.bottom = getComputedStyle(sliderBar).height
 }
 
-percentInput.addEventListener("input", (e) => {
-	var bars = document.querySelectorAll(".percent-slider-bar");
+function initStorageCapacityPercent() {
+	var sliderBars = document.querySelector(".percent-slider-bars");
+	percentSliderStorage.style.height = getComputedStyle(sliderBars).height;
+	percentSliderStorage.style.bottom = getComputedStyle(sliderBars).height
+}
 
+function percentChange(bar1, bar2, e) {
 	var applied = e.target.value + "%";
 	var remaining = Number(100 - e.target.value) + "%";
 
 	// left side of pct slider
-	bars[0].style.width = bars[0].innerHTML = applied;
-
+	bar1.style.width = bar1.innerHTML = applied;
 	// right side of pct slider
-	bars[1].style.width = bars[1].innerHTML = remaining;
+	bar2.style.width = bar2.innerHTML = remaining;
+}
 
+percentNewRecordInput.addEventListener("input", (e) => {
+	percentChange(newRecordSliderBar1, newRecordSliderBar2, e);
 	updateSignalSignaturerMessage();
 });
 
-initPercentInput()
+percentSliderStorage.addEventListener("input", (e) => {
+	percentChange(serverStorageSliderBar1, serverStorageSliderBar2, e);
+	updateSignalSignaturerMessage();
+});
+
+initSliderPercentInput(newRecordSliderBar1, percentNewRecordInput);
+initSliderPercentInput(serverStorageSliderBar1, percentSliderStorage);
